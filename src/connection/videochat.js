d17a7429b5dc344074beac1ac35fb6f2372dc80c
arihantjain8118 <arihantjain8118@gmail.com>
2021-03-18 00:41:11 +0530

    fixed turn server

--- src/connection/videochat.js
+++ src/connection/videochat.js
@@ -111,6 +111,27 @@ function VideoChatApp(props) {
     const peer = new Peer({
       initiator: false,
       trickle: false,
+      config: {
+
+        iceServers: [
+            { 
+                urls: 'stun:stun.l.google.com:19302' 
+            }, 
+            { 
+                urls: 'stun:global.stun.twilio.com:3478?transport=udp' 
+            },
+            {
+                urls: "stun:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            },
+            {
+                urls: "turn:numb.viagenie.ca",
+                username: "sultan1640@gmail.com",
+                credential: "98376683"
+            }
+        ]
+    },
       stream: stream
     });
     peer.on("signal", data => {
